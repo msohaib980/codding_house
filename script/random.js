601bead46c067d3bded07fa53ac1f6e7d879f6fd
mskhanmohmandE <msohaib980@gmail.com>
2015-11-01 15:39:04 -0500

    added project files

---
+++ script/random.js
@@ -0,0 +1,12 @@
+
+
+
+function getRandomNumber(lower,upper){
+	if (isNaN(lower) || isNaN(upper)){
+		throw new Error("welll, on eentry or all is not a valid number")}
+
+	return Math.floor(Math.random()*(upper-lower+1))+lower;
+}
+console.log(getRandomNumber("w",20))
+
+
